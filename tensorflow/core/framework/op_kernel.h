c4d2f44cc25ea80471d5897d82c49af1cc8c4b9d
Wenda Zhou <zhouwenda@outlook.com>
2018-11-14 16:03:39 -0500

    fix OpArgIterator dereference operator

--- tensorflow/core/framework/op_kernel.h
+++ tensorflow/core/framework/op_kernel.h
@@ -402,8 +402,8 @@ class OpArgIterator {
     return old_value;
   }
 
-  reference operator*() { return (*list_)[i_]; }
-  pointer operator->() { return &(*list_)[i_]; }
+  reference operator*() const { return (*list_)[i_]; }
+  pointer operator->() const { return &(*list_)[i_]; }
 
  private:
   const ListType* const list_;
